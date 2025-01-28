6edc07ae24d4f32ab07f9ac798ebd17fc4b2ab8c
Hugo GG <mategomez04.hg@gmail.com>
2025-01-28 15:26:54 -0600

    formulario_registro_funcional
    Agregando funcionalidad a formulario de registro

--- app/src/main/java/com/example/formularios_v2/MainActivity.java
+++ app/src/main/java/com/example/formularios_v2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.formularios_v2;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +19,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        EditText editTextNombre = findViewById(R.id.editTextText);
+        EditText editTextEmail = findViewById(R.id.editTextText2);
+        Button buttonRegistrar = findViewById(R.id.button);
+
+
+        buttonRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nombre = editTextNombre.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+
+                // Validaciones
+                if (nombre.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Por favor, ingresa tu nombre", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(MainActivity.this, "Por favor, ingresa un email válido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                String mensajeFinal = "Nombre: " + nombre + "\nEmail: " + email;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Registro Exitoso")
+                        .setMessage(mensajeFinal)
+                        .setPositiveButton("Aceptar", null)
+                        .show();
+            }
         });
     }
 }
