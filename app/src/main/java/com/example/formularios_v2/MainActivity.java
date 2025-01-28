c9febdf2939a943c0f909d99739c952de1a8809b
Hugo GG <mategomez04.hg@gmail.com>
2025-01-28 15:16:49 -0600

    Encuesta_preferencias_funcional
    Agregando funcionalidad a encuesta de preferencias

--- app/src/main/java/com/example/formularios_v2/MainActivity.java
+++ app/src/main/java/com/example/formularios_v2/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.formularios_v2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +18,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText editTextColorFavorito = findViewById(R.id.editTextText4);
+        EditText editTextComidaFavorita = findViewById(R.id.editTextText5);
+        Button buttonEnviar = findViewById(R.id.button6);
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String colorFavorito = editTextColorFavorito.getText().toString().trim();
+                String comidaFavorita = editTextComidaFavorita.getText().toString().trim();
+
+                // Validaciones
+                if (colorFavorito.isEmpty() || comidaFavorita.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Mostrar los datos en un cuadro de diálogo
+                String mensajeFinal = "Color Favorito: " + colorFavorito +
+                        "\nComida Favorita: " + comidaFavorita;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Encuesta de Preferencias")
+                        .setMessage(mensajeFinal)
+                        .setPositiveButton("Aceptar", null)
+                        .show();
+            }
         });
     }
 }
